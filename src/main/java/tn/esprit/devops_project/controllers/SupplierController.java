6a7ce1b1c5a276f044a506fec2d0946b6bfe09ca
iamponil <ameur.nemlaghi@hotmail.fr>
2023-11-15 23:08:09 +0100

    fix:docker-compose

--- src/main/java/tn/esprit/devops_project/controllers/SupplierController.java
+++ src/main/java/tn/esprit/devops_project/controllers/SupplierController.java
@@ -2,7 +2,6 @@ package tn.esprit.devops_project.controllers;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.devops_project.dto.SupplierDTO;
 import tn.esprit.devops_project.entities.Supplier;
 import tn.esprit.devops_project.services.services_interface.ISupplierService;
 import java.util.List;
