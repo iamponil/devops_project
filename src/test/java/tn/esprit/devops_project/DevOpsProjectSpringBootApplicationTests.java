2db6b4a640b0d286f54d3ec009485f63e806c1c1
iamponil <ameur.nemlaghi@hotmail.fr>
2023-11-16 07:48:09 +0100

    feat:code coverage

--- src/test/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplicationTests.java
+++ src/test/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplicationTests.java
@@ -1,13 +1,21 @@
 package tn.esprit.devops_project;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 @SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
 class DevOpsProjectSpringBootApplicationTests {
 
     @Test
-    void contextLoads() {
-        // Simple test to check if the Spring context loads successfully
+    void aVoidmain(CapturedOutput output) {
+        DevOpsProjectSpringBootApplication.main(new String[]{});
+
+        // Check if the expected string is present in the output
+        Assertions.assertThat(output).contains("Started DevOpsProjectSpringBootApplication");
     }
 }
