9c319ee7dbad5613115de309497a5aaffc0a27b7
iamponil <ameur.nemlaghi@hotmail.fr>
2023-11-16 07:39:23 +0100

    feat:code coverage

---
+++ src/test/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.devops_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Simple test to check if the Spring context loads successfully
+    }
+}
